025eeb95aeef08ebad46bc069bec4b24787d4b93
dyggo <dyggo@DESKTOP-NEOQB0Q>
2022-01-13 15:41:37 -0300

    Adicionar arquivos de projeto.

---
+++ BancoConta/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace BancoConta
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente (int agencia, int numeroDaConta)
+        {
+            Agencia = agencia;
+            NumeroConta = numeroDaConta;
+            TotalDeContasCriadas++;
+        }
+        public static int TotalDeContasCriadas {get; private set;}
+        public Cliente Titular {get; set;}
+        public int NumeroConta {get; set;}
+        private int _agencia; 
+        public int Agencia 
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+
+        private double _saldo;
+        public double Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                if (_saldo < 0)
+                    {return;}
+                else
+                    { _saldo = value;}
+            }
+        }
+        
+
+        public bool Sacar(double valor)
+        {
+            if (valor > this._saldo)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+
+        }
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor > _saldo)
+            {
+                return false;
+            }
+            contaDestino.Depositar(valor);
+            _saldo -= valor;
+            return true;
+        }
+    }
+}
---
+++ BancoConta/Test.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoConta
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoSeth =  new ContaCorrente(303, 201529);
+            Cliente seth = new Cliente();
+            seth.Nome = "Seth";
+            seth.CPF = "043.959.791-90";
+            seth.Profissao = "Pro-Wrestler";
+
+            contaDoSeth.Titular = seth;
+            contaDoSeth.Saldo = 500.0;
+
+            Cliente roman = new Cliente();
+            roman.Nome = "Roman";
+            roman.CPF = "296.054.231-20";
+            roman.Profissao = "Pro-Wrestler";
+
+            ContaCorrente contaDoRoman = new ContaCorrente(020, 201530);
+            contaDoRoman.Titular = roman;
+            contaDoRoman.Saldo = 1000.0;
+
+
+            Console.WriteLine("Nome: " + contaDoSeth.Titular.Nome);
+            Console.WriteLine("Agencia: " + contaDoSeth.Agencia);
+            Console.WriteLine("Numero da Conta: " + contaDoSeth.NumeroConta);
+            Console.WriteLine("Saldo: " + contaDoSeth.Saldo);
+            Console.WriteLine("CPF: " + seth.CPF);
+
+            Console.WriteLine();
+            
+            Console.WriteLine("Nome: " + contaDoRoman.Titular.Nome);
+            Console.WriteLine("Agencia: " + contaDoRoman.Agencia);
+            Console.WriteLine("Numero da Conta: " + contaDoRoman.NumeroConta);
+            Console.WriteLine("Saldo: " + contaDoRoman.Saldo);
+            Console.WriteLine("CPF: " + roman.CPF);
+
+            contaDoSeth.Sacar(100);
+            contaDoSeth.Depositar(100);
+            Console.WriteLine();
+            Console.WriteLine("Saldo do Seth é: " + contaDoSeth.Saldo);
+            Console.WriteLine("Saldo do Roman é: " + contaDoRoman.Saldo);
+
+            int valorTransferencia = 100;
+            contaDoRoman.Transferir(valorTransferencia, contaDoSeth);
+            Console.WriteLine();
+            Console.WriteLine("O valor da transferencia de Roman para Seth é: " + valorTransferencia);
+            Console.WriteLine("Saldo do Seth é: " + contaDoSeth.Saldo);
+            Console.WriteLine("Saldo do Roman é: " + contaDoRoman.Saldo);
+
+            Console.WriteLine();
+            Console.WriteLine("O total de contas criadas no Banco foi de " + ContaCorrente.TotalDeContasCriadas + " contas");
+
+            Console.ReadLine();
+
+         
+        }
+    }
+}
